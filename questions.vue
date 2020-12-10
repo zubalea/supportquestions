75f076ef8c9395ece7814ff365ef13ca5953b9c4
zubalea <zubalea@users.noreply.github.com>
2020-12-10 13:15:42 -0500

    Update questions.vue

--- questions.vue
+++ questions.vue
@@ -8,53 +8,17 @@ The DATA via an api.
     {
         "id": "020d540c-8006-40bd-890a-f2a74a3a4d0b",
         "parent": "1c047169-5eae-400e-ab02-4dd4bd503790",
-        "tag": "2e7eb990-3b0e-11eb-8728-15519f5e3c10",
-        "icon": "",
         "title": "My New Dash",
-        "group": null,
-        "url": "2e7eb990-3b0e-11eb-8728-15519f5e3c10",
-        "type": "L",
-        "category": "U",
-        "customer": null,
-        "user": null,
-        "access_groups": [],
-        "services": [],
-        "sort_order": 10,
-        "version": 1607620819553678
     },
     {
         "id": "1c047169-5eae-400e-ab02-4dd4bd503790",
         "parent": null,
-        "tag": "dashboard",
-        "icon": "mdi-view-dashboard",
         "title": "dashboard",
-        "group": "/dashboard",
-        "url": null,
-        "type": "H",
-        "category": "S",
-        "customer": null,
-        "user": null,
-        "access_groups": [],
-        "services": [],
-        "sort_order": 1,
-        "version": 1607617504553902
     },
     {
         "id": "ae3c7aa8-a49e-4bbb-9ca2-b7725d8e8e1a",
         "parent": "1c047169-5eae-400e-ab02-4dd4bd503790",
-        "tag": "global",
-        "icon": null,
         "title": "dash_global",
-        "group": null,
-        "url": "global",
-        "type": "L",
-        "category": "S",
-        "customer": null,
-        "user": null,
-        "access_groups": [],
-        "services": [],
-        "sort_order": 1,
-        "version": 1607620346544090
     }
 ]
 
